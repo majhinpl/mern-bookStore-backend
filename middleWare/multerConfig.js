b6aada012143a44c20817d627aed41f518c5e7cf
nepstech <majhintech@gmail.com>
2024-03-13 21:08:13 +0300

    limits file size

--- middleWare/multerConfig.js
+++ middleWare/multerConfig.js
@@ -3,16 +3,20 @@ const multer = require('multer')
 const storage = multer.diskStorage({
     destination : function(req,file,cb){
         const allowedFileTypes = ['image/png','image/jpeg','image/jpg']
-        if(!allowedFileTypes.includes(file.mimetype)){
+        const fileSize = parseInt(req.headers["content-length"])
+
+        if(!allowedFileTypes.includes(file.mimetype) && fileSize <= 1024 * 1024){
             cb(new Error("This filetype is not supported")) // cb(error)
             return
         }
         cb(null,'./storage') // --> cb(error,success)
     },
-    filename : function(req,file,cb){
-        cb(null,Date.now()+ "-"+ file.originalname)
+    filename : function(req, file, cb){
+        cb(null, Date.now() + "-"+ file.originalname)
     }
-})
+});
+
+
 
 module.exports = {
     storage,
