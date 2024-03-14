d09a813a9140e0562eb56767c4aff7cc9e5073af
nepstech <majhintech@gmail.com>
2024-03-14 20:18:28 +0300

    working on file size

--- middleWare/multerConfig.js
+++ middleWare/multerConfig.js
@@ -3,8 +3,8 @@ const multer = require('multer')
 const storage = multer.diskStorage({
     destination : function(req,file,cb){
         const allowedFileTypes = ['image/png','image/jpeg','image/jpg'];
-        const fileSize = parseInt(req.headers["content-length"]);
-        if(!allowedFileTypes.includes(file.mimetype) && fileSize >= 1048576){
+        // const fileSize = parseInt(req.headers["content-length"]);
+        if(!allowedFileTypes.includes(file.mimetype) && file.size > 1024 * 1024){
             cb(new Error("This filetype is not supported")) // cb(error)
             return
         }
